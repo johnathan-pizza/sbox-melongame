3b87e1279fa95be128a39e3ff796d4c245b377c9
johnathan-pizza <qweebmcdonalds44@gmail.com>
2022-01-31 19:31:31 -0800

    changed prop to watermelon

--- code/Pawn.cs
+++ code/Pawn.cs
@@ -55,7 +55,7 @@ namespace Sandbox
 			if ( IsServer && Input.Pressed( InputButton.Attack1 ) )
 			{
 				var ragdoll = new ModelEntity();
-				ragdoll.SetModel( "models/citizen/citizen.vmdl" );
+				ragdoll.SetModel( "models/sbox_props/watermelon/watermelon.vmdl" );
 				ragdoll.Position = EyePos + EyeRot.Forward * 40;
 				ragdoll.Rotation = Rotation.LookAt( Vector3.Random.Normal );
 				ragdoll.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
